943c5478f546b90863174ff520023aa999f6b0bf
Samuel Lee <leesamuel423@gmail.com>
2024-07-25 16:06:53 -0400

    remove

--- lua/sam/treesitter.lua
+++
@@ -1,9 +0,0 @@
-local M = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-treesitter/playground",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
-}
-
-return { M }
